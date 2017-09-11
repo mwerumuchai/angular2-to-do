8de4ba7f8747f1c07fd604f2aa180d20fbfdd004
mwerumuchai <mwerumuchai@gmail.com>
2017-09-11 16:18:04 +0300

    ex1: using template to display properties

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,10 +5,24 @@ import { Component } from '@angular/core';
   template: `
   <div class="container">
     <h1> My First Angular 2 App </h1>
+    <h3>One of my favorite band is: {{ favoriteBand }}</h3>
+    <p>If I had to choose a favorite painter it would be: {{ favoritePainter }}</p>
+    <p>The number of slices of pie I would like is: {{ slicesOfPie }}</p>
+    <h3>One of my favorite albums is: </h3>
+    <p>{{ favoriteAlbum.title }}</p>
+    <p>By {{ favoriteAlbum.artist }}</p>
+    <p>Released in {{ favoriteAlbum.released }}</p>
   </div>
   `
 })
 
 export class AppComponent {
+  favoriteBand: string = 'Ani DiFranco';
+  favoritePainter: string = 'Van Gogh';
+  slicesOfPie: number = 3;
+  favoriteAlbum: Album = new Album("Disintegration", "The Cure", 1989);
+}
 
+export class Album {
+  constructor (public title: string, public artist: string, public released: number) {  }
 }
