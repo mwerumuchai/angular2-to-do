cb27838a3e238130f5f1cbe5cdd1da102299fe4f
mwerumuchai <mwerumuchai@gmail.com>
2017-09-11 17:22:08 +0300

    built-in Event emmitters

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,7 +5,7 @@ import { Component } from '@angular/core';
   template: `
   <div class="container">
     <h1>My First Angular 2 App</h1>
-    <h3 *ngFor="let currentTask of tasks">{{ currentTask.description }}</h3>
+    <h3 (click)="doStuff(currentTask)" *ngFor="let currentTask of tasks">{{ currentTask.description }}</h3>
   </div>
   `
 })
@@ -17,9 +17,21 @@ export class AppComponent {
       new Task("Rewatch all the Lord of the Rings movies.", 2),
       new Task("Do the laundry.", 3)
   ];
+  doStuff(clickedTask: Task) {
+    if(clickedTask.done === true) {
+      alert("This task is done!");
+    } else {
+      alert("This task is not completed yet! Get to work!");
+    }
+  }
 }
 
-export class Task {       //Task object have 3 properties
-  public done: boolean = false;  //task 1: boolean value named done and its default is false
-  constructor(public description: string, public id: number) {   }    //task 2: description TASK 3: an Id to assign each task a unique number
+export class Task {
+  public done: boolean = false;
+  constructor(public description: string, public id: number) {   }
 }
+
+/*{{ }}:moustache tags: they cause any expression/variables inside of the double curly brackets to be evaluatedso that their value is displayed
+ task 1: boolean value named done and its default is false
+ task 2: description TASK 3: an Id to assign each task a unique number
+ use an output bunding when you want to trigger an event from a DOM element*/
